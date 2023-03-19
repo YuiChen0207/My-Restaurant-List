7a248f5e89c3309316980d7ef81d5387ad33c056
YuiChen0207 <asd0935252359@gmail.com>
2023-03-19 17:46:41 +0800

    Add restaurant list and searching bar, when clicked the picture of restaurent would show up the detail

---
+++ app.js
@@ -0,0 +1,48 @@
+// require packages used in the project
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const port = 3000;
+const restaurantList = require("./restaurant.json");
+
+// setting template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// setting static files
+app.use(express.static("public"));
+
+// routes setting
+app.get("/", (req, res) => {
+  res.render("index", { restaurants: restaurantList.results });
+});
+
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  const restaurantId = req.params.restaurant_id;
+  const showRestaurant = restaurantList.results.find(
+    (restaurant) => restaurant.id.toString() === restaurantId
+  );
+  res.render("show", { restaurant: showRestaurant });
+});
+
+//若是找不到keyword則alert並返回上一頁
+//在node下使用js語法alert及window.history.back()達到相同效果
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword.toLowerCase().trim();
+  if (!keyword.length) return;
+  const restaurants = restaurantList.results.filter((restaurant) =>
+    restaurant.name.toLowerCase().includes(keyword)
+  );
+  if (!restaurants.length) {
+    return res.send(
+      "<script>alert('Can not find " +
+        keyword +
+        "');window.history.back();</script>"
+    );
+  }
+  res.render("index", { restaurants: restaurants });
+});
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`);
+});
