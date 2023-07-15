5b6e29e4f9589ea77e25090d0661c0fd13fe2622
YuiChen0207 <asd0935252359@gmail.com>
2023-07-15 19:46:55 +0800

    add rwd and router post subscribe

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const axios = require("axios");
 const express = require("express");
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
 const app = express();
 const port = 3000;
@@ -21,7 +23,19 @@ app.engine(
 );
 
 app.set("view engine", "handlebars");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static("public"));
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "asd0935252359@gmail.com",
+    pass: "inoolwoglrgwrpkx",
+  },
+});
 
 app.get("/", async (req, res) => {
   try {
@@ -96,7 +110,6 @@ app.get("/restaurants/:restaurant_name", async (req, res) => {
 
   res.render("show", {
     restaurant: showRestaurant,
-    restaurants: restaurantList,
     slicedRestaurants,
   });
 });
@@ -111,6 +124,34 @@ app.get("/search", (req, res) => {
       restaurant.category.toLowerCase().includes(keyword)
   );
   res.render("index", { restaurants, keyword });
+});
+
+app.post("/subscribe", (req, res) => {
+  const email = req.body.email;
+
+  const displayedRestaurants = restaurantList.slice(0, 13);
+
+  const mailOptions = {
+    from: "asd0935252359@gmail.com",
+    to: email,
+    subject: "Subscription Confirmation",
+    text: "Thank you for subscribing!",
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+      res.render("index", {
+        subscriptionStatus: "Subscription failed",
+        restaurants: displayedRestaurants,
+      });
+    } else {
+      res.render("index", {
+        subscriptionStatus: "Subscription successful",
+        restaurants: displayedRestaurants,
+      });
+    }
+  });
 });
 
 app.listen(port, () => {
